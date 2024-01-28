dbe3789551e53579db51645efb7617ab6ea09d0b
trtushar35 <trtushar35@gmail.com>
2024-01-29 05:01:45 +0600

    include header-navbar, create user, store and show in the list, upload image, install notify package

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('role');
+            $table->integer('phone');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('address');
+            $table->string('image')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.master');
-});
+Route::get('/',[HomeController::class, 'home'])->name('dashboard');
+
+Route::get('/users/list',[UserController::class, 'list'])->name('users.list');
+Route::get('/create/user',[UserController::class, 'create'])->name('create.user');
+Route::post('/store/user',[UserController::class, 'store'])->name('store.user');
