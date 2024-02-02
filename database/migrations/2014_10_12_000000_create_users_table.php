fd1809e199d0dd6098dbd89a67fccfe696a2fed3
sabihaParvin <parvinjui536@gmail.com>
2024-02-02 19:22:27 +0600

    role permission

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-<<<<<<< HEAD
             $table->string('role');
             $table->integer('phone')->nullable();
-=======
-            $table->foreignId('role_id')->constrained('roles')->restrictOnDelete();
-            $table->integer('phone');
->>>>>>> 5a217196c20bc89afb82fe51ace72244b2afb1ed
             $table->string('email')->unique();
             $table->string('address')->nullable();
             $table->string('image')->nullable();
